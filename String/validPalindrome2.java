0a7583e6d047cc06f90e688b381f02010be4aadd
vinayrewatkar <vinayrewatkar.257@gmail.com>
2024-11-29 10:22:11 +0530

    valid palindrome 2

---
+++ String/validPalindrome2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                // Try skipping either the left or the right character
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    // Helper function to check if a substring is a palindrome
+    private boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
