8844a9e11f73429760355139767cbff363fa06af
vinayrewatkar <vinayrewatkar.257@gmail.com>
2024-12-03 14:09:20 +0530

    climbing stairs

---
+++ Dynamic programming/climbingStairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+
+    public int solve(int n, int[] dp) {
+        if (n == 1 || n == 0)
+            return 1;
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+}
