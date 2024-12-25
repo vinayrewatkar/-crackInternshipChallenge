5fe18d0015db856d2358ca5954b13f0b94db797e
vinayrewatkar <vinayrewatkar.257@gmail.com>
2024-12-25 16:30:56 +0530

    sudoko solver

---
+++ backtracking/sudoko.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[row][col] = '.'; // Undo if solution is not valid
+                        }
+                    }
+                    return false; // No valid number found
+                }
+            }
+        }
+        return true; // Sudoku solved successfully
+    }
+
+    public boolean isValid(char[][] board, int row, int col, char num) {
+        // Check row
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check subgrid (3x3)
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + startRow][j + startCol] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
